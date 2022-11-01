cc28b44aec6f9ca863c57ff5f4e792b644520b59
Minindu Senadheera <69930656+MininduSenadheera@users.noreply.github.com>
2022-11-01 18:37:49 +0530

    page ui with functionality created

--- app.py
+++ app.py
@@ -133,3 +133,57 @@ def predictPrice(neighbourhood,property_type,room_type,accommodates,bedrooms,bat
 
     prediction = regressor.predict([[neighbourhood,property_type,room_type,accommodates,bathrooms,bedrooms,beds,bed_type,availability_365,cancellation_policy]])
     return prediction 
+
+def main():
+    price = 0
+    
+    st.set_page_config(page_title='Airbnb Price Predictor',layout='wide', page_icon=None,  initial_sidebar_state="auto", menu_items=None)
+    col1, col2, col3= st.columns(gap="large",spec=[1.5,1,4])
+
+    with col1:
+        neighbourhoods = ['Roslindale', 'Jamaica Plain', 'Mission Hill', 'Fenway/Kenmore','Back Bay', 'Leather District', 'Chinatown', 'Hyde Park','North End', 'Roxbury', 'South End', 'Mattapan', 'East Boston','South Boston', 'Charlestown', 'West End', 'Beacon Hill','Theater District', 'Downtown Crossing', 'Downtown','Financial District', 'Government Center', 'Allston-Brighton','West Roxbury', 'Chestnut Hill', 'Dorchester', 'Brookline','Cambridge', 'Somerville', 'Harvard Square']
+        neighbourhood = st.selectbox(label='Select Neighbourhood', options=neighbourhoods)
+
+        property_types = ['House', 'Apartment', 'Condominium', 'Villa', 'Bed & Breakfast','Town house', 'Entire Floor', 'Loft', 'Guest house', 'Boat', 'Dorm','Other', 'Unknown']
+        property_type = st.selectbox(label='Select Property Type', options=property_types)
+
+        room_types = ['Entire home/apt', 'Private room', 'Shared room']
+        room_type = st.selectbox(label='Select Room Type', options=room_types)
+
+        accommodates = st.number_input(label='No.of accommodates', min_value=1, max_value=16, format='%d')
+
+        bedrooms = st.number_input(label='No.of bedrooms', min_value=1, max_value=5,format='%d')
+
+        bathrooms = st.number_input(label='No.of bathrooms', min_value=0.0, max_value=6.0, step=0.5, format='%.1f')
+
+        beds = st.number_input(label='No.of beds', min_value=1, max_value=16)
+
+        bed_types = ['Real Bed', 'Pull-out Sofa', 'Futon', 'Airbed', 'Couch']
+        bed_type = st.selectbox(label='Select Bed Type', options=bed_types)
+
+        availability_365 = st.number_input(label='No.of days available in a year', min_value=1, max_value=365, format='%d')
+
+        cancellation_policies = ['moderate', 'flexible', 'strict', 'super strict']
+        cancellation_policy = st.selectbox(label='Select Cancellation Policy', options=cancellation_policies)
+
+    with col3:
+        
+        image = Image.open('./public/airbnb.png')
+        st.image(image, width=250)
+
+        st.markdown(
+            "<h5 >Predict a price for your Airbnb based on what you are providing to customers</h5><br>"
+            , unsafe_allow_html=True)
+
+        if st.button(label="Predict Price"):
+            price = predictPrice(neighbourhood,property_type,room_type,accommodates,bathrooms,bedrooms,beds,bed_type,availability_365,cancellation_policy) 
+            price = str(price)[1:-1]
+            price = "{:.2f}".format(float(price))
+            st.header(f"Estimated Price is: ${price}")
+    
+    with col2:
+        pass
+            
+if __name__=='__main__':
+    main()
+
